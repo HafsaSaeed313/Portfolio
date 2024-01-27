c41424e8857a28b14b7780952b395dd7618f508e
Hafsa <135526388+HafsaSaeed313@users.noreply.github.com>
2024-01-27 06:40:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function  toggleMenu() {
+  let menu = document.querySelector(".menu-links");
+  let icon = document.querySelector(".humburger-icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
